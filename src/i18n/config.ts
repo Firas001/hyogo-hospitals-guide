c361fc206a49b8c8b12eff1da0e8c7ecaa46479f
Firas001 <al.dweni001@gmail.com>
2026-04-10 00:56:01 +0900

    fix: update auto lang

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
 export type Locale = (typeof locales)[number];
 
 export const locales = ['ar', 'en', 'bn', "ja", "fr", "sw"] as const;
-export const defaultLocale: Locale = 'ar';
+export const defaultLocale: Locale = 'en';
